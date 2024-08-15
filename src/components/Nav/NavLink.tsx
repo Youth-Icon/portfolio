0014e1abec072e50eb32c502eba5f8469ad03c84
Bashamega <adambashaahmednaji@gmail.com>
2024-08-15 11:37:21 +0300

    Nav Link

--- src/components/Nav/NavLink.tsx
+++ src/components/Nav/NavLink.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export function NavLink({
   text,
   href,
@@ -7,5 +9,15 @@ export function NavLink({
   href: string;
   className?: string;
 }) {
-  return <></>;
+  return (
+    <Link
+      href={href}
+      className={
+        "text-gray-300 whitespace-nowrap inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 " +
+        className
+      }
+    >
+      {text}
+    </Link>
+  );
 }
