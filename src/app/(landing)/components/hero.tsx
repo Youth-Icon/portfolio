9ca79398e4cfe88bb1a1d6fa852d7582c13ce71e
Bashamega <adambashaahmednaji@gmail.com>
2024-08-15 12:11:50 +0300

    Hero

---
+++ src/app/(landing)/components/hero.tsx
@@ -0,0 +1,8 @@
+
+export function Hero(){
+    return(
+        <section className="w-screen h-screen relative">
+            <div className="absolute -z-20 top-0 left-0 w-full h-[96vh] bg-hero-pattern"></div>
+        </section>
+    )
+}
