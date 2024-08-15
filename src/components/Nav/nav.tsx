88f50dc72d67edc53a4f992f630f32d6048a25a2
Bashamega <adambashaahmednaji@gmail.com>
2024-08-15 11:55:26 +0300

    Added Buttons

--- src/components/Nav/nav.tsx
+++ src/components/Nav/nav.tsx
@@ -3,11 +3,14 @@ import { NavLink } from "./NavLink";
 
 export function Nav() {
   return (
-    <nav>
+    <nav className=" flex justify-between items-center w-screen py-5 px-10">
       <div>
         <Logo />
       </div>
-      <div></div>
+      <div className=" flex justify-between lg:w-1/5">
+        <NavLink href="/#" text="About Us"/>
+        <NavLink href="/#" text="Get Started" className="bg-white !text-black hover:bg-primary/90" />
+      </div>
     </nav>
   );
 }
