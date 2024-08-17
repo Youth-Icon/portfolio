f75b199dcc461e10a1ead44feb1d1078c8131fb8
Bashamega <adambashaahmednaji@gmail.com>
2024-08-17 12:52:40 +0300

    claimer fix

--- src/app/(landing)/components/claimer.tsx
+++ src/app/(landing)/components/claimer.tsx
@@ -1,6 +1,6 @@
 export function Claimer() {
   return (
-    <form className=" lg:flex my-10 justify-center lg:space-y-0 space-y-5">
+    <form className=" lg:flex my-10 lg:w-4/5 lg:ml-[10%] justify-center lg:space-y-0 space-y-5">
       <div className=" border rounded-[12px] overflow-hidden flex w-full">
         <input placeholder="username" className=" bg-transparent p-4 w-full" spellCheck={false} name="username"></input>
         <p className="md:px-4 bg-neutral-400 bg-opacity-30 backdrop-blur-3xl font-manrope text-neutral-400 p-4">.xprofile.me</p>
